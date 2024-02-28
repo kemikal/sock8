83ce3ceb2cbfd48188fd6e320d9e23cc234d2edb
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-02-28 13:37:07 +0100

    :sparkles: feat(server) get all users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../lib/conn.js')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET all users*/ 
+router.get('/', function(req, res) {
+ connection.connect((err)=> {
+  if(err) console.log(err)
+
+    let query = `SELECT *
+                 FROM users`;
+  connection.query(query , (err, result) => {
+    if(err) console.log(err)
+    let newResult = Object.keys(result).length // Checks the lenght of the result
+
+    if(newResult == 0) {
+      res.status(404).json({message: 'No users exist'})
+    } else {
+      res.json(result)
+      console.log(result)
+    }
+  })
+ })
 });
 
 
