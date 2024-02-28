586a5795607201e8e6d47156967cda5c05baebe4
YeseniaCM <yeseniacamilla@hotmail.com>
2024-02-28 13:22:09 +0100

    ✨ feat(server) mysql2, cors installations

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const connection = require('./lib/conn.js')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+connection.connect(function(err){
+    if(err) throw err
+    else console.log("Uppkopplad till databasen");
+  })
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
