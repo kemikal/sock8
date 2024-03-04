e4f5a52e74a2f872e12eec62684e62aea69c82a8
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-04 09:29:54 +0100

    DO

--- server/lib/conn.js
+++ server/lib/conn.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 connection = mysql.createConnection({
-    host: "localhost",
+    host: "192.168.50.182",
     port:"3306",
     user: "chat_room",
     password: "DTb5-(nff.pnA0NH",
