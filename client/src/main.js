7c33fc65779495522c5f923ae2c38a1678130558
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-03-04 08:39:34 +0100

    DO

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import { printchat } from './chat.js';
 import { logInForm } from './printLogInBtn.js';
 import { logOutBtn } from './printLogOutBtn.js';
 
-export let app = document.getElementById('app');
+export let app = document.querySelector('#app');
 export let formDiv = document.createElement('div'); // kunde inte nå ''document'' i dem andra mopdulerna , la det i main och imorterade från main
 
 if(localStorage.getItem('user')){
